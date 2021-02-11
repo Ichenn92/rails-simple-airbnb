c37167d325550b12a83ef7ee3d49bd6194d875cc
Tristan Hörler <tristan.horler@gmail.com>
2021-02-11 19:16:42 +0100

    add bootstraop jquery popper

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'An amazing and multicultural place in Shanghai',
+  address: 'Shanghai',
+  description: '认真吗 你真的是来这里翻译的吗?',
+  price_per_night: 110,
+  number_of_guests: 7
+)
+Flat.create!(
+  name: 'Affordable and cosy night in Lausanne',
+  address: 'Lausanne',
+  description: 'In the suburb of Lausanne, get the freshiest air of the Leman lake',
+  price_per_night: 55,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Bed and Breakfast of Paris',
+  address: 'Paris',
+  description: 'The most charming place of the entire Paris',
+  price_per_night: 175,
+  number_of_guests: 4
+)
